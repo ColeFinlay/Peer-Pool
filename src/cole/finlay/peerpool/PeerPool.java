1735d59c38a2abd51c4350b272b77db6673d1b0f
Cole Finlay <fint.colman@gmail.com>
2011-06-26 19:11:45 +0100

    initial commit

---
+++ src/cole/finlay/peerpool/PeerPool.java
@@ -0,0 +1,13 @@
+package cole.finlay.peerpool;
+
+import cole.finlay.peerpool.bitcoinbasic.BasicClient;
+
+public class PeerPool {
+	
+	public static void main(String[] args) {
+		
+		BasicClient bc = new BasicClient();
+		
+	}
+
+}
